190ee561301eb9c57688b2a513ee7b78ec760b25
RossTarrant <rosstarrant3@gmail.com>
2022-01-26 07:26:59 +0000

    Updated index, styles and script with functionality for etch a sketch

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-function initalise(){
+function createGrid(squares){
     const content = document.querySelector('.content');
-    for(let i = 0; i <16; i++){
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    const boxSize = 640/squares;
+    for(let i = 0; i <squares; i++){
         const row = document.createElement('div');
         row.classList.add('row');
-        for(let i = 0; i <16; i++){
+        for(let i = 0; i <squares; i++){
             const box = document.createElement('div');
             box.classList.add('box');
+            box.style.width = boxSize+"px";
+            box.style.height = boxSize+"px";
             box.addEventListener('mouseover', () =>{
                 box.style.backgroundColor = "black";
             })
             row.appendChild(box);
         }
-        content.appendChild(row);
+        grid.appendChild(row);
+        content.appendChild(grid);    
     }
 }
-initalise()
+function resetGrid(){
+    let gridSize = prompt("How many squares per side would you like? (Max 100)");
+    const grid = document.querySelector('.grid');
+    grid.remove();
+    createGrid(gridSize);
+}
+createGrid(16);
+const resetBtn = document.querySelector("button");
+resetBtn.addEventListener("click", () => {
+    resetGrid();
+})
